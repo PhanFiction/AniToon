8437df96c53e27424f643ef9fe90e461b03385e9
Steve <stevewu24@gmail.com>
2024-09-02 22:01:01 -0700

    added devise, google oAuth

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :validatable, 
+         :omniauthable, omniauth_providers: [:google_oauth2]
+
+  def self.from_omniauth(auth)
+    data = auth.info
+    find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
+      user.email = data.email
+      user.password = Devise.friendly_token[0, 20]
+      user.username = data.username ? data.username : data.first_name + data.last_name # if no username, make username with first and last name
+      user.full_name = data.name
+      user.avatar_url = data.image
+      # If you are using confirmable and the provider(s) you use validate emails,
+      # uncomment the line below to skip the confirmation emails.
+      user.skip_confirmation!
+    end
+  end
+end
