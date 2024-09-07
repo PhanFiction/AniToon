f0f77044c4eaf3a385da1d2d4c44825e77ee0dae
Steve <stevewu24@gmail.com>
2024-09-06 22:11:27 -0700

    updated bookmark controller and bookmark model

---
+++ app/controllers/api/bookmarks_controller.rb
@@ -0,0 +1,33 @@
+class Api::BookmarksController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    user = User.find(current_user.id)
+    bookmarks = user.bookmarks
+    render json: bookmarks
+  end
+
+  def create 
+    user = User.find(current_user.id)
+    bookmark = user.bookmarks.build(bookmark_params)
+    if bookmark.save
+      render json: { success: "Bookmark has been saved", data: bookmark }
+    else
+      render json: { error: bookmark.errors.full_messages }, status: 500
+    end
+  end
+
+  def destroy
+    bookmark = Bookmark.find(params[:id]) # search bookmark by id
+    
+    if bookmark.destroy # delete bookmark
+      render json: { success: "Bookmark has been deleted", redirect: root_url }
+    else
+      render json: { error: bookmark.errors.full_messages }, status: 500
+    end
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:name, :description, :anime_id, :poster, :mal_id, :rating)
+  end
+end
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,5 +2,9 @@ class Bookmark < ApplicationRecord
   belongs_to :user
 
   validates :anime_id, presence: true
-  validates :title, presence: true
+  validates :name, presence: true
+
+  def as_json(options = {})
+    super({}.merge(options))
+  end
 end
