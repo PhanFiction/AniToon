c18961958a54cc9017860d42c1f1838a8609461e
Steve <stevewu24@gmail.com>
2024-09-19 20:48:31 -0700

    created App.jsx

--- app/javascript/frontend/src/App.jsx
+++ app/javascript/frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Episode from "./pages/Episode";
+import Anime from "./pages/Anime";
+import Categorization from "./pages/Categorization";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/anime/category/:id" element={<Categorization />} />
+        <Route path="/anime/genre/:id" element={<Categorization />} />
+        <Route path="/anime/watch/:id" element={<Episode />} />
+        <Route path="/anime/info" element={<Anime />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </Router>
