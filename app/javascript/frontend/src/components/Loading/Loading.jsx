685741b209a2ce5e23256296fb28aeeea3eecf26
Steve <stevewu24@gmail.com>
2024-09-27 23:17:51 -0700

    created loading component and added loading component styles

---
+++ app/javascript/frontend/src/components/Loading/Loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Loading() {
+  return (
+    <div className="loader">
+      <div data-glitch="Loading..." className="glitch">Loading...</div>
+    </div>
+  )
+}
