e303e76428118a02085884260958879f52bb9527
Steve <stevewu24@gmail.com>
2024-09-28 23:10:49 -0700

    added lazy loading and suspense

--- app/javascript/frontend/src/App.jsx
+++ app/javascript/frontend/src/App.jsx
@@ -1,13 +1,81 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Episode from "./pages/Episode";
-import Anime from "./pages/Anime";
-import Categorization from "./pages/Categorization";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React, { lazy, Suspense } from 'react';
+import Nav from "./components/Nav/Nav";
+// import Home from "./pages/Home";
+// import Episode from "./pages/Episode";
+// import Anime from "./pages/Anime";
+// import Categorization from "./pages/Categorization";
+
+/* 
+Todo
+  1. Figure out how to deal with suspending component on render when user clicks on different links 
+  2. Fix caption padding in VideoPlayer component
+*/
+
+const Home = lazy(() => import('./pages/Home'));
+const Categorization = lazy(() => import('./pages/Categorization'));
+const Episode = lazy(() => import('./pages/Episode'));
+const Anime = lazy(() => import('./pages/Anime'));
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
+      <Nav />
+      <div className="mb-20 mt-28">
+        <Routes>
+          <Route
+            path="/anime/category/:id"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <Categorization />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/anime/genre/:id"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <Categorization />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/anime/watch/:id"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <Episode />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/anime/info"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <Anime />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <Home />
+              </Suspense>
+            }
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
+/* function App() {
+  return (
+    <BrowserRouter>
+    <Nav />
+    <div className="mb-20 mt-28">
       <Routes>
         <Route path="/anime/category/:id" element={<Categorization />} />
         <Route path="/anime/genre/:id" element={<Categorization />} />
@@ -15,8 +83,8 @@ function App() {
         <Route path="/anime/info" element={<Anime />} />
         <Route path="/" element={<Home />} />
       </Routes>
-    </Router>
-  )
+    </div>
+    </BrowserRouter>
+  );
 }
-
-export default App;
+ */
