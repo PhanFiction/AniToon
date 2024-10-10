5cefc73ba033f27014fbfc3959780c90713b4434
Steve <stevewu24@gmail.com>
2024-10-09 22:47:09 -0700

    Updated Card component  - Updated styling

--- app/javascript/frontend/src/components/Card/Card.jsx
+++ app/javascript/frontend/src/components/Card/Card.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 export default function Card({ showType, backgroundImg, title }){
   return (
-    <article className="group w-28 md:w-40">
+    <article className="w-full h-full group">
       <div className="flex flex-col relative h-full mb-2 group bg-darkBluePurple text-lime-300 hover:-translate-y-1 duration-500"
       >
-        <div className="w-full h-full absolute left-0 top-0 group-hover:bg-[#8988885c] group-hover:backdrop-blur-[6px] duration-500">
+        <div className="w-full h-full absolute left-0 top-0 rounded-md group-hover:bg-[#8988885c] group-hover:backdrop-blur-[6px] duration-500">
           <p className="font-bold absolute bottom-2 left-2 opacity-0 duration-300 group-hover:opacity-100">{showType}</p>
           <svg
-            className="h-[1.5em] w-[1.5em] absolute top-28 left-[68px] duration-300 opacity-0 group-hover:opacity-100"
+            className="h-[1.5em] w-[1.5em] absolute top-24 md:top-28 left-[53px] md:left-[4.7em] duration-300 opacity-0 group-hover:opacity-100"
             viewBox="0 0 48 48"
             fill="none"
             height="36"
@@ -31,9 +31,9 @@ export default function Card({ showType, backgroundImg, title }){
           </svg>
           <svg className="absolute top-2 right-2 opacity-0 duration-300 group-hover:opacity-100" stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" height="1.2em" width="1.2em" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"></path></svg>
         </div>
-        <img src={backgroundImg} alt={backgroundImg} className="w-full h-full object-cover" />
+        <img src={backgroundImg} alt={backgroundImg} className="w-full h-full object-cover rounded-md" />
       </div>
-      <p className="group-hover:text-red text-xs md:text-base font-medium duration-500 w-28 md:w-40 truncate">{title}</p>
+      <p className="group-hover:text-red text-xs md:text-base font-medium duration-500 w-30 md:w-40 truncate">{title}</p>
     </article>
   );
 };
