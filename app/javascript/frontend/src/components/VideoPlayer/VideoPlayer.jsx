dcddbee005c68b1688d78120dec30f5b0644c186
Steve <stevewu24@gmail.com>
2024-10-17 10:55:21 -0700

    Created videoplayer component

---
+++ app/javascript/frontend/src/components/VideoPlayer/VideoPlayer.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect, useRef } from 'react';
+import ReactPlayer from 'react-player';
+
+export default function VideoPlayer({ video, captions }) {
+/*   const [load, setLoad] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoad(true)
+    }, 500);
+
+    return () => {
+      setLoad(false);
+    }
+  }, [video]);
+
+  if (!load) {
+    return <span>loading...</span>
+  }
+ */
+
+  const emptyVideo = () => (
+    <div className="w-full h-full">
+
+    </div>
+  )
+
+  return (
+    <ReactPlayer
+      config={{
+        file: {
+          attributes: {
+            crossOrigin: "true",
+          },
+          tracks: captions,
+        },
+      }}
+      onReady={() => console.log('ready ')}
+      onStart={() => console.log('start ')}
+      url={video}
+      fallback={emptyVideo()}
+      controls={true}
+      width="100%"
+      height="100%"
+    />
+  );
+}
