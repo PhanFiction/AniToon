518624e0ae06989820c4317b74b0cf9f7115feaf
Steve <stevewu24@gmail.com>
2024-10-23 23:43:47 -0700

    Created pagination component

---
+++ app/javascript/frontend/src/components/Paginate/Paginate.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Pagination from 'rc-pagination';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+export default function Paginate({ pages=1 }) {
+  const location = useLocation();
+  const url = location.pathname;
+  const navigate = useNavigate();
+
+  const currentPage = location.search.split('=')[1];
+
+  const handlePageChange = (page) => {
+    navigate(`${url}?page=${page}`);
+  };
+
+  return (
+    <div className="flex justify-center text-red">
+      <Pagination
+        className="mt-8"
+        onChange={handlePageChange}
+        defaultCurrent={currentPage}
+        pageSize={1}
+        total={pages}
+        onShowSizeChange={false}
+        showPrevNextJumpers={false}
+      />
+    </div>
+  )
+}
