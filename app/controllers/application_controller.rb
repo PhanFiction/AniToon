8328d3d5e1dabc7a2eba512edebc5a815e461956
Steve <stevewu24@gmail.com>
2024-11-20 18:19:21 -0800

    updated application controller for backend  - Added redirect after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :full_name])
+  end
+
+  def after_sign_in_path_for(resource)
+    "http://localhost:3000" # Redirect to your React app after sign in for Devise
+  end
 end
