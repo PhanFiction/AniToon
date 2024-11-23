d0bc9249d7c25c37897ccd365b3e3a064274198d
Steve <stevewu24@gmail.com>
2024-11-22 23:19:27 -0800

    Updated nav component

--- app/javascript/frontend/src/components/Nav/Nav.jsx
+++ app/javascript/frontend/src/components/Nav/Nav.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import SearchBar from '../SearchBar/SearchBar';
 import { Link } from 'react-router-dom';
 import logo from '../../../assets/logo.png';
