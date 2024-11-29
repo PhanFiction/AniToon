76db61ae85377e05df0497b302344ddf3149f014
Steve <stevewu24@gmail.com>
2024-11-28 23:10:54 -0800

    Added submit button

--- app/javascript/frontend/src/components/Comment/Comment.jsx
+++ app/javascript/frontend/src/components/Comment/Comment.jsx
@@ -5,9 +5,12 @@ export default function Comment() {
     <div className="flex items-center">
       <img src="https://cdn.noitatnemucod.net/avatar/100x100/jujutsu_kaisen/File14.png" alt="profile" className="rounded-full w-16 h-16" />
       <div className="flex flex-col">
-        <span>Comment as user</span>
+        <span className="">Comment as user</span>
         <label htmlFor="comment"></label>
         <textarea name="comment" id="comment" className="resize-none text-black w-96 p-2" />
+        <button className="ml-auto mt-4">
+          Submit
+        </button>
       </div>
     </div>
   )
