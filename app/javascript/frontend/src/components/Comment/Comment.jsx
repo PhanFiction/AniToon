57f61b8f502a6ba27a628a3777ae935daa1685b8
Steve <stevewu24@gmail.com>
2024-12-02 23:10:30 -0800

    Updated comment component

--- app/javascript/frontend/src/components/Comment/Comment.jsx
+++ app/javascript/frontend/src/components/Comment/Comment.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
+import CommentForm from './CommentForm';
+import CommentItem from './CommentItem';
 
 export default function Comment() {
   return (
-    <div className="flex items-center">
-      <img src="https://cdn.noitatnemucod.net/avatar/100x100/jujutsu_kaisen/File14.png" alt="profile" className="rounded-full w-16 h-16" />
-      <div className="flex flex-col">
-        <span className="">Comment as user</span>
-        <label htmlFor="comment"></label>
-        <textarea name="comment" id="comment" className="resize-none text-black w-96 p-2" />
-        <button className="ml-auto mt-4">
-          Submit
-        </button>
-      </div>
-    </div>
+    <section>
+      <CommentForm />
+      <CommentItem />
+    </section>
   )
 }
