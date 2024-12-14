e777fb696c367994415f55492d5fa1f526401353
Steve <stevewu24@gmail.com>
2024-12-13 17:43:58 -0800

    updated comment component  - Added ternary to show commentform component if user is logged in

--- app/javascript/frontend/src/components/Comment/Comment.jsx
+++ app/javascript/frontend/src/components/Comment/Comment.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import CommentForm from './CommentForm';
 import CommentItem from './CommentItem';
+import useFetchUser from '../../hooks/useFetchUser';
 
 export default function Comment() {
+  const { user } = useFetchUser();
   return (
     <section>
-      <CommentForm />
+      {
+        user ? <CommentForm /> : null
+      }
       <CommentItem />
     </section>
   )
