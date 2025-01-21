6ea03a97b377325d26d119ec895ee1743360d064
Steve <stevewu24@gmail.com>
2025-01-20 23:38:24 -0800

    Updated Home component  - Updated homeData state,  - Added fetch custom hook for user,  - Updated Layout

--- app/javascript/frontend/src/pages/Home.jsx
+++ app/javascript/frontend/src/pages/Home.jsx
@@ -1,22 +1,30 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Swiper, SwiperSlide } from 'swiper/react';
-import Loading from "../components/Loading";
 import AnimeItem from "../components/Card/AnimeItem";
+import useFetchUser from "../hooks/useFetchUser";
+import LoadingCard from "../components/Loading/LoadingCard";
 
 function Home() {
-  const [homeData, setHomeData] = useState([]);
+  const [homeData, setHomeData] = useState({
+    trendingAnimes: [],
+    latestCompletedAnimes: [],
+    top10Animes: { week: [] },
+    mostFavoriteAnimes: [],
+    mostPopularAnimes: [],
+    latestEpisodeAnimes: [],
+    topUpcomingAnimes: [],
+  });
   const [loading, setLoading] = useState(true);
   const [watchList, setWatchList] = useState([]);
+  const { user } = useFetchUser();
   
   useEffect(() => {
     const fetchHomeData = async () => {
       const res = await fetch('/api/anime');
       const homeData = await res.json();
-      //console.log(homeData);
       setHomeData(homeData.data);
       setLoading(false);
-      //console.log(homeData.data);
       if (homeData.watch_list?.length > 0) setWatchList(homeData.watch_list);
     }
     fetchHomeData();
@@ -25,6 +33,14 @@ function Home() {
       setLoading(true);
     }
   }, []);
+
+  function fakeLoadCards() {
+    return (
+      Array.from({length: 5}).map((_, i) => (
+        <LoadingCard key={i} />
+      ))
+    )
+  }
 
   return (
     <section className="">
@@ -36,10 +52,9 @@ function Home() {
         >
           {
             loading ? <p>Loading</p> :
-            homeData.spotlightAnimes.map((item, index) => 
+            homeData.spotlightAnimes.map(item => 
               <SwiperSlide key={item.id}>
                 <div className="w-full h-full relative">
-
                   <div className="flex flex-col gap-4 w-full lg:w-1/2 absolute bottom-0 bg-darkBluePurple p-4 opacity-90">
                     <h1 className="text-md md:text-2xl font-bold">{item.name}</h1>
                     <p className="hidden md:block md:w-4/5 h-30 text-sm overflow-hidden my-4">{item.description.slice(0, 425)}...</p>
@@ -90,9 +105,7 @@ function Home() {
           }}
         >
           {
-            loading
-            ? <Loading />
-            : homeData.trendingAnimes.map(item =>
+            homeData.trendingAnimes.map(item =>
               <SwiperSlide key={item.id}>
                 <div className="w-full h-full relative">
                   <div className="flex flex-col gap-4 w-full absolute bottom-0 bg-darkBluePurple p-4 opacity-90">
@@ -114,78 +127,75 @@ function Home() {
         </Swiper>
       </article>
 
-      {/* Change to columns of 4 in the future */}
-      <article className="pl-2 md:pl-8 mt-8">
-        <h1 className="text-2xl font-bold ml-4 md:ml-0 mb-4">
-          Top Airing
-        </h1>
-        <div className="flex flex-wrap justify-center md:justify-start gap-4 mb-2">
-          {
-            loading
-            ? <Loading />
-            : homeData.top10Animes.week.slice(0,5).map(item =>
-              <AnimeItem item={item} key={item.id} bookmarked={watchList}/>
-            )
-          }
-        </div>
-        <Link to="/anime/category/top-airing?page=1" className="text-gray text-xs font-bold ml-5 md:ml-0">
-          View More &gt;
-        </Link>
-      </article>
+      <div class="flex flex-wrap justify-evenly">
+        <article className="pl-2 md:pl-8 mt-8">
+          <h1 className="text-lg md:text-2xl font-bold ml-4 md:ml-0 mb-4">
+            Top Airing
+          </h1>
+          <div className="flex flex-col flex-wrap justify-center md:justify-start gap-4 mb-2">
+            {
+              loading ? fakeLoadCards() :
+              homeData.top10Animes.week.slice(0,5).map(item =>
+                <AnimeItem item={item} key={item.id} bookmarked={watchList} user={user} />
+              )
+            }
+          </div>
+          <Link to="/anime/category/top-airing?page=1" className="text-gray text-xs font-bold ml-5 md:ml-0">
+            View More &gt;
+          </Link>
+        </article>
 
-      <article className="pl-2 md:pl-8 mt-8">
-        <h1 className="text-2xl font-bold ml-4 md:ml-0 mb-4">
-          Latest Completed
-        </h1>
-        <div className="flex flex-wrap justify-center md:justify-start gap-4 mb-2">
-          {
-            loading
-            ? <Loading />
-            : homeData.latestCompletedAnimes.map(item =>
-              <AnimeItem item={item} key={item.id} bookmarked={watchList}/>
-            )
-          }
-        </div>
-        <Link to="/anime/category/completed?page=1" className="text-gray text-xs font-bold ml-5 md:ml-0">
-          View More &gt;
-        </Link>
-      </article>
+        <article className="pl-2 md:pl-8 mt-8">
+          <h1 className="text-lg md:text-2xl font-bold ml-4 md:ml-0 mb-4">
+            Completed
+          </h1>
+          <div className="flex flex-col flex-wrap justify-center md:justify-start gap-4 mb-2">
+            {
+              loading ? fakeLoadCards() :
+              homeData.latestCompletedAnimes.map(item =>
+                <AnimeItem item={item} key={item.id} bookmarked={watchList} user={user} />
+              )
+            }
+          </div>
+          <Link to="/anime/category/completed?page=1" className="text-gray text-xs font-bold ml-5 md:ml-0">
+            View More &gt;
+          </Link>
+        </article>
 
-      <article className="pl-2 md:pl-8 mt-8">
-        <h1 className="text-2xl font-bold ml-4 md:ml-0 mb-4">
-          Most Favorite
-        </h1>
-        <div className="flex flex-wrap justify-center md:justify-start gap-4 mb-2">
-          {
-            loading
-            ? <Loading />
-            : homeData.mostFavoriteAnimes.map(item =>
-              <AnimeItem item={item} key={item.id} bookmarked={watchList}/>
-            )
-          }
-        </div>
-        <Link to="/anime/category/most-favorite?page=1" className="text-gray text-xs font-bold ml-5 md:ml-0">
-          View More &gt;
-        </Link>
-      </article>
+        <article className="pl-2 md:pl-8 mt-8">
+          <h1 className="text-lg md:text-2xl font-bold ml-4 md:ml-0 mb-4">
+            Most Favorite
+          </h1>
+          <div className="flex flex-col flex-wrap justify-center md:justify-start gap-4 mb-2">
+            {
+              loading ? fakeLoadCards() :
+              homeData.mostFavoriteAnimes.map(item =>
+                <AnimeItem item={item} key={item.id} bookmarked={watchList} user={user} />
+              )
+            }
+          </div>
+          <Link to="/anime/category/most-favorite?page=1" className="text-gray text-xs font-bold ml-5 md:ml-0">
+            View More &gt;
+          </Link>
+        </article>
 
-      <article className="pl-2 md:pl-8 mt-8">
-        <h1 className="text-2xl font-bold ml-4 md:ml-0 mb-4">
-          Latest Completed
-        </h1>
-        <div className="flex flex-wrap justify-center md:justify-start gap-4 mb-2">
-          {
-            loading
-            ? <Loading />
-            : homeData.mostPopularAnimes.map(item =>
-              <AnimeItem item={item} key={item.id} bookmarked={watchList}/>
-            )
-          }
-        </div>
-        <Link to="/anime/category/completed?page=1" className="text-gray text-xs font-bold ml-5 md:ml-0">
-          View More &gt;
-        </Link>
-      </article>
+        <article className="pl-2 md:pl-8 mt-8">
+          <h1 className="text-lg md:text-2xl font-bold ml-4 md:ml-0 mb-4">
+            Most Popular
+          </h1>
+          <div className="flex flex-col flex-wrap justify-center md:justify-start gap-4 mb-2">
+            {
+              loading ? fakeLoadCards() :
+              homeData.mostPopularAnimes.map(item =>
+                <AnimeItem item={item} key={item.id} bookmarked={watchList} user={user} />
+              )
+            }
+          </div>
+          <Link to="/anime/category/completed?page=1" className="text-gray text-xs font-bold ml-5 md:ml-0">
+            View More &gt;
+          </Link>
+        </article>
+      </div>
 
       <article className="pl-2 md:pl-8 mt-8">
         <h1 className="text-2xl font-bold ml-4 md:ml-0 mb-4 w-48">
@@ -193,10 +203,9 @@ function Home() {
         </h1>
         <div className="flex flex-wrap justify-center md:justify-start gap-4 mb-2">
           {
-            loading
-            ? <Loading />
-            : homeData.latestEpisodeAnimes.map(item =>
-              <AnimeItem item={item} key={item.id} bookmarked={watchList}/>
+            loading ? fakeLoadCards() :
+            homeData.latestEpisodeAnimes.map(item =>
+              <AnimeItem item={item} key={item.id} bookmarked={watchList} user={user} />
             )
           }
         </div>
@@ -211,10 +220,9 @@ function Home() {
         </h1>
         <div className="flex flex-wrap justify-center md:justify-start gap-4 mb-2">
           {
-            loading
-            ? <Loading />
-            : homeData.topUpcomingAnimes.map(item =>
-              <AnimeItem item={item} key={item.id} bookmarked={watchList}/>
+            loading ? fakeLoadCards() :
+            homeData.topUpcomingAnimes.map(item =>
+              <AnimeItem item={item} key={item.id} bookmarked={watchList} user={user} />
             )
           }
         </div>
